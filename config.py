6f0b216ba48c6ea5ae3f17cc420e5e58a04a37ab
odogwukelly <odogwukellyice@gmail.com>
2024-06-22 18:22:04 +0100

    hot fix on config file

--- config.py
+++ config.py
@@ -9,3 +9,10 @@ class Config():
     SECRET_KEY = os.environ.get("SECRET_KEY")
     SQLALCHEMY_DATABASE_URI = os.getenv("DB_URI")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ENGINE_OPTIONS = {
+        'connect_args': {
+            'connect_timeout': 20
+        },
+        'pool_recycle': 1800,
+        'pool_pre_ping': True,
+    }
